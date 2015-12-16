f365f545242d7c98687dfb206757521ca912dba8
dpacheco <david.pacheco@beubi.com>
2015-12-16 15:04:24 +0000

    BEEWEB-30

--- src/octoprint/server/api/bee_custom.py
+++ src/octoprint/server/api/bee_custom.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-import time
+import os
 from octoprint.server.util.iwlistparse import get_ssid_list
 
 __author__ = "BEEVC - Electronic Systems "
@@ -10,7 +10,9 @@ __copyright__ = "Copyright (C) 2014 The OctoPrint Project - Released under terms
 
 from octoprint.server.api import api
 from octoprint.server import printer, NO_CONTENT
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, make_response
+
+WIFI_CMODE_SCRIPT = 'wifi_client_mode.sh'
 
 #~~ BVC custom API
 api = Blueprint("beeapi", __name__)
@@ -39,6 +41,42 @@ def getAvailableHotspots():
 
 @api.route("/wifi/connect", methods=["POST"])
 def connectNetwork():
+
+	if not "application/json" in request.headers["Content-Type"]:
+		return make_response("Expected content-type JSON", 400)
+
+	data = request.json
+
+	# writes the wpa_supplicant configuration file
+	import re
+	regex_net = re.compile(r'ssid=".+"', re.IGNORECASE)
+	regex_pass = re.compile(r'psk=".+"', re.IGNORECASE)
+
+	lines = []
+	with open('/etc/wpa_supplicant/wpa_supplicant.conf') as infile:
+		for line in infile:
+			if 'ssid' in line:
+				line = regex_net.sub('ssid="%s"' % data['network'], line)
+			if 'psk' in line:
+				line = regex_pass.sub('psk="%s"' % data['password'], line)
+
+			lines.append(line)
+
+	from os.path import expanduser
+	home = expanduser("~")
+	with open(home + '/wpa_supplicant_update.conf', 'w') as outfile:
+		for line in lines:
+			outfile.write(line)
+
+	# Executes the shell script to change the wi-fi mode
+	import os.path
+	script_path = home + '/' + WIFI_CMODE_SCRIPT
+	if os.path.isfile(script_path):
+		try:
+			import subprocess
+			subprocess.call([script_path])
+		except:
+			print ('Error executing wi-fi client mode script.')
 
 	return NO_CONTENT
 
