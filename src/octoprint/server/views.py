bd96c22e59d288bd0d2edb5ae9902b69b1c68bbf
dpacheco <david.pacheco@beubi.com>
2016-10-18 17:08:28 +0100

    BEEWEB-110 Fix wrong formatting from Help menu

--- src/octoprint/server/views.py
+++ src/octoprint/server/views.py
@@ -172,7 +172,7 @@ def index():
 	templates["navbar"]["entries"] = dict(
 		settings=dict(template="navbar/settings.jinja2", _div="navbar_settings", styles=["display: none"], data_bind="visible: loginState.isAdmin"),
 		maintenance=dict(template="navbar/maintenance.jinja2", _div="navbar_maintenance"),
-		help=dict(template="navbar/help.jinja2", _div="navbar_help")
+		help=dict(template="navbar/help.jinja2", _div="navbar_help", classes=["dropdown"])
 	)
 	if enable_accesscontrol:
 		templates["navbar"]["entries"]["login"] = dict(template="navbar/login.jinja2", _div="navbar_login", classes=["dropdown"], custom_bindings=False)
