f720e284e82ebef9b01c586ba8beedf919dc565e
dpacheco <david.pacheco@beubi.com>
2016-11-29 17:08:11 +0000

    BEEWEB-119 Changed files div sidebar position

--- src/octoprint/settings.py
+++ src/octoprint/settings.py
@@ -233,7 +233,7 @@ default_settings = {
 		"components": {
 			"order": {
 				"navbar": ["maintenance", "settings", "systemmenu", "help", "login"],
-				"sidebar": ["workbench_controls", "state", "files", "connection"],
+				"sidebar": ["files", "workbench_controls", "state", "connection"],
 				"tab": ["workbench", "gcodeviewer", "control", "temperature", "terminal", "timelapse"],
 				"settings": [
 					"section_printer", "printerprofiles", "temperatures", "terminalfilters", "gcodescripts",
